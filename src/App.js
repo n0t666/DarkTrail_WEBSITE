67fcea873b8264849b305a0be0942a74a45bf6c4
Leandroooo! <50177@aebatalha.ccems.pt>
2023-05-20 17:52:38 +0100

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as  Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router,Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages";
 import About from "./pages/about"
