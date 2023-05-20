8bb3854f6ece05bc99e17832a6d79687a7c11696
Leandroooo! <50177@aebatalha.ccems.pt>
2023-05-20 03:28:11 +0100

    added contact form

---
+++ src/components/contactForm.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { useForm } from "@formcarry/react";
+function ContactForm() {
+  // Call the "useForm" hook in your function component
+  const { state, submit } = useForm({
+    id: "z9zntFXUFI",
+  });
+
+  // Success message
+  if (state.submitted) {
+    return <div>Thank you! We received your submission.</div>;
+  }
+
+  return (
+    <form className="mt-6" onSubmit={submit}>
+      <div className="flex-1">
+        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+          Nome Completo
+        </label>
+        <input
+          required
+          name="name"
+          type="text"
+          placeholder="John Doe"
+          className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+        />
+      </div>
+      <div className="flex-1 mt-6">
+        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+          Endereço Email
+        </label>
+        <input
+          required
+          name="email"
+          type="email"
+          placeholder="johndoe@example.com"
+          className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+        />
+      </div>
+      <div className="w-full mt-6">
+        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">
+          Mensagem:
+        </label>
+        <textarea
+          required
+          name="msg"
+          className="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+          placeholder="corpo da mensagem"
+          defaultValue={""}
+        />
+      </div>
+      <button className="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50" type="submit">
+        CONTACTAR
+      </button>
+    </form>
+  );
+}
+
+export default ContactForm;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,3 +1,4 @@
+import ContactForm  from '../components/contactForm'
 export const Contact = () => {
   return (
      <section className="bg-white dark:bg-gray-900">
@@ -47,23 +48,7 @@ export const Contact = () => {
             </div>
             <div className="mt-8 lg:w-1/2 lg:mx-6">
               <div className="w-full px-8 py-10 mx-auto overflow-hidden bg-white rounded-lg shadow-2xl dark:bg-gray-900 lg:max-w-xl shadow-gray-300/50 dark:shadow-black/50">
-                <form className="mt-6">
-                  <div className="flex-1">
-                    <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Nome Completo</label>
-                    <input type="text" placeholder="John Doe" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
-                  </div>
-                  <div className="flex-1 mt-6">
-                    <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Endereço Email</label>
-                    <input type="email" placeholder="johndoe@example.com" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
-                  </div>
-                  <div className="w-full mt-6">
-                    <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Mensagem:</label>
-                    <textarea className="block w-full h-32 px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md md:h-48 dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="corpo da mensagem" defaultValue={""} />
-                  </div>
-                  <button className="w-full px-6 py-3 mt-6 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                    CONTACTAR
-                  </button>
-                </form>
+              <ContactForm/>
               </div>
             </div>
           </div>
